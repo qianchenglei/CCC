2d6b2931e4a5f2ec2b7d7de6ec121ba1a490e7d5
qianchenglei <1426674267@qq.cim>
2026-05-26 17:14:51 +0800

    power

--- leix.c
+++ leix.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 #include <math.h>
-int shuixianhuashu(n){
-    
+int power(int n, int strs){
+    int ret = 1;
+    while (n-->0)
+    {
+        ret *= strs; 
+    }
+    return ret;
 }
 
 int main(){
     int n;
-    printf("please input a number in 100~9999999'\n'");
+    printf("please input a number in 3~7\n");
     scanf("%d",&n);
-
+    int strnum = power(n-1,10);
+    int endnum = power(n,10)-1;
+    printf("%d\n",strnum);
+    printf("%d",endnum);
     return 0;
 }
