af32b603d746c86c7d2f62db78fc553f2a13cb45
qianchenglei <1426674267@qq.cim>
2026-05-26 16:44:04 +0800

    shuixianhashu1

---
+++ leix.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int shuixianhuashu(n){
+    
+}
+
+int main(){
+    int n;
+    printf("please input a number in 100~9999999'\n'");
+    scanf("%d",&n);
+
+    return 0;
+}
