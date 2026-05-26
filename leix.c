87ae9972de150db9ea9a4909dddc304830a1b2bb
qianchenglei <1426674267@qq.cim>
2026-05-26 17:42:54 +0800

    end

--- leix.c
+++ leix.c
@@ -1,21 +1,42 @@
 #include <stdio.h>
 #include <math.h>
-int power(int n, int strs){
+int power(int n, int strs)
+{
     int ret = 1;
-    while (n-->0)
+    while (n-- > 0)
     {
-        ret *= strs; 
+        ret *= strs;
     }
     return ret;
 }
 
-int main(){
+int main()
+{
     int n;
     printf("please input a number in 3~7\n");
-    scanf("%d",&n);
-    int strnum = power(n-1,10);
-    int endnum = power(n,10)-1;
-    printf("%d\n",strnum);
-    printf("%d",endnum);
+    scanf("%d", &n);
+    int strnum = power(n - 1, 10);
+    int endnum = power(n, 10) - 1;
+
+    // printf("%d\n",strnum);
+    // printf("%d",endnum);
+    for (int i = strnum; i < endnum; i++)
+    {
+        int cnt = n;
+        int plusnum = 0;
+        int ans = 0;
+        int temp = i;
+        for (int j = 0; j < cnt; j++)
+        {
+            plusnum = power(n, (temp % 10));
+            ans += plusnum;
+            temp /= 10;
+        }
+        if (i == ans)
+        {
+            printf("%d ", ans);
+        }
+    }
+
     return 0;
 }
