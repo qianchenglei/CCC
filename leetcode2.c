a5c7c2bb7ffba3d4779462e53ba9f48fb9568582
qianchenglei <1426674267@qq.cim>
2026-05-27 20:05:17 +0800

    n相求和end

--- leetcode2.c
+++ leetcode2.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
-#include <stdbool.h>
- struct TreeNode {
-   int val;
-    struct TreeNode *left;
-    struct TreeNode *right;
- };
+// #include <stdio.h>
+// #include <stdbool.h>
+//  struct TreeNode {
+//    int val;
+//     struct TreeNode *left;
+//     struct TreeNode *right;
+//  };
 
-bool checkTree(struct TreeNode* root) {
+// bool checkTree(struct TreeNode* root) {
 
     
-}
+// }
 
-int main() {
+// int main() {
     
-}
+// }
